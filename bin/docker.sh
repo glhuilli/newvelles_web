a298540f514b77506f2566b9e36fbfbb3dfc2ebd
Gaston L Huillier <glhuilli@gmail.com>
2021-06-04 10:02:14 -0700

    Updated version and notes for deployment

--- bin/docker.sh
+++ bin/docker.sh
@@ -1,5 +1,8 @@
-# rebuild the container with new code
-docker build -t newvelles-web-container .
+# rebuild the container with new code (version X)
+docker build -t newvelles-web-container:vX .
 
 # run container locally
-# docker run -p 5000:5000 newvelles-web-container
+docker run -p 5000:5000 newvelles-web-container:vX
+
+# revisit docker images 
+docker images
--- bin/lightsail.sh
+++ bin/lightsail.sh
@@ -1,8 +1,8 @@
 # Create lightsail container service MICRO
 aws lightsail create-container-service --service-name newvelles-web-service --power micro --scale 1
 
-# Push Docker container to lightsail container
-aws lightsail push-container-image --service-name newvelles-web-service --label newvelles-web-container --image newvelles-web-container
+# Push Docker container to lightsail container (version X)
+aws lightsail push-container-image --service-name newvelles-web-service --label newvelles-web-container --image newvelles-web-container:vX
 
 # Deploy container to lightsail 
 aws lightsail create-container-service-deployment --service-name newvelles-web-service --containers file://containers.json --public-endpoint file://public-endpoint.json
