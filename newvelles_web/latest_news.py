5a61da8f8e486b878d3f970475ffb269c6913b88
Gaston L Huillier <glhuilli@gmail.com>
2021-07-31 09:21:31 -0700

    Final fix

--- newvelles_web/latest_news.py
+++ newvelles_web/latest_news.py
@@ -1,4 +1,6 @@
+import collections
 import json
+import re
 from typing import Optional
 import urllib.request
 
@@ -7,12 +9,23 @@ LATEST_NEWS_URI = 'https://public-newvelles-data-bucket.s3-us-west-2.amazonaws.c
 LOCAL_NEWS = './data/latest_news.json'
 
 
+def _clean_string(s):
+    return s.replace('"', '').replace('\'', '-')
+
+
+def map_nested_dicts(ob, func):
+    if isinstance(ob, collections.Mapping):
+        return {_clean_string(k): map_nested_dicts(v, func) for k, v in ob.items()}
+    else:
+        return func(ob)
+
+
 def _escape_news(news_dict):
     """
     Assume the string that needs to be scaped is the title
     """
-    scaped_dic = json.dumps(news_dict).replace('\'', '-')
-    return json.loads(scaped_dic)
+    scaped_dic = json.dumps(news_dict)
+    return map_nested_dicts(json.loads(scaped_dic), lambda x: _clean_string(x))
 
 
 def get_latest_news(local: Optional[bool] = False):
